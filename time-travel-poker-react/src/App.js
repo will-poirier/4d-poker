b46a83621264da3f23c86b728446bde8944b1e3a
will-poirier <theyellowgreninja@gmail.com>
2023-03-22 17:09:51 -0400

    mild progress on ui -- there's a hand now

--- time-travel-poker-react/src/App.js
+++ time-travel-poker-react/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Card from './Card';
+import Hand from './Hand';
 
 function App() {
+    // debug init code
+    let cards = [new Card({suit: 'Spades', value:'14'}), new Card({suit: 'Hearts', value:'9'})]
+
     return (
         <div className="App">
-            <Card suit='Spades' value='14'></Card>
+            <Card suit='Diamonds' value='11'></Card>
+            <Hand cards={cards}></Hand>
         </div>
     );
 }
--- time-travel-poker-react/src/Hand.js
+++ time-travel-poker-react/src/Hand.js
@@ -4,12 +4,18 @@ class Hand extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            cards: [],
-            size: 0
+            cards: props.cards,
+            size: 5
         }
     }
 
     render = () => {
-        
+        let cardRenders = []
+        for (let index in this.state.cards) {
+            cardRenders.push(this.state.cards[index].render())
+        }
+        return <div className='hand'>{cardRenders}</div>
     }
 }
+
+export default Hand;
